2ffbcaddc7e85a8939502ed2bca1985088f360c6
Satheesh CK <v-skrishnankutty@egencia.com>
2016-02-01 18:26:07 +0530

    firstcamelprj

---
+++ firstcamelprj/src/main/java/com/javavillage/firstcamelprj/FirstRouteBuilder.java
@@ -0,0 +1,15 @@
+package com.javavillage.firstcamelprj;
+
+import org.apache.camel.builder.RouteBuilder;
+
+public class FirstRouteBuilder extends RouteBuilder{
+
+	@Override
+	public void configure() throws Exception {
+		
+	from("file:D:/WS/CAMEL_WS/camel/input?noop=true")
+	.process(new LogProcessor())
+	.bean(new Transormer(),"transformContent")
+	.to("file:D:/WS/CAMEL_WS/camel/output");	
+	}
+}
